7299881eecccdf17d727360f442be87f5bb52d05
ardiansetya <ardiansetya129@gmail.com>
2025-08-31 19:50:28 +0700

    fix: entry point

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,16 @@
-import { AuthProvider } from "@/context/AuthContext";
+import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
 import "./global.css";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  const { isAuthenticated } = useAuth();
+  if (!isAuthenticated) {
+    return <Redirect href="/(auth)/login" />;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
