f9e7c4df3cb5145037133415c14da8744ec50995
ardiansetya <ardiansetya129@gmail.com>
2025-09-01 14:48:23 +0700

    fix err object

--- app/index.tsx
+++ app/index.tsx
@@ -28,7 +28,7 @@ import { useQueryClient } from "@tanstack/react-query";
     const [loginSuccess, setLoginSuccess] = useState(false);
     const queryClient = useQueryClient();
 
-    console.log(error);
+    const stringErr = JSON.stringify(error);
 
 
     const {
@@ -127,9 +127,7 @@ import { useQueryClient } from "@tanstack/react-query";
             {error && (
               <View className="mb-6 bg-red-50 border border-red-200 rounded-lg p-3">
                 <Text className="text-red-600 text-center text-sm">
-                  {typeof error === "string"
-                    ? error
-                    : error?.message || "Terjadi Kesalahan"}
+                  {typeof error === "string" ? error : stringErr}
                 </Text>
               </View>
             )}
